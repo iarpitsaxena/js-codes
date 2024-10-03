927dc21b7e0694dbef480fcf2a2bb7e9af970563
iarpitsaxena <the.arpit.saxena@gmail.com>
2024-10-03 22:39:50 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // will treat all js code as newer version
+
+//alert(3 + 3); // will give error as we are using nodejs not browser
+
+/*
+number
+big int
+string
+boolean => true/false
+null => standalone keyword
+undefined
+object
+symbol => used to define unique
+
+
+
+ */
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
