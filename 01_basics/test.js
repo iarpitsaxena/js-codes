49f72b30face13ba67ccc8c890c285f36872c69b
iarpitsaxena <the.arpit.saxena@gmail.com>
2024-10-03 22:21:12 +0530

    added note

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,3 +1,7 @@
-console.log("Arpit Saxena")
+console.log("Arpit Saxena");
 
-console.log("hi hello hey")
+console.log("hi hello hey");
+
+/*
+prefer not to use var because of issue of block scope and functional scope.
+*/
